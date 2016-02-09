3f1b88262510123c66621016a6bf0a13016d55e2
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-10 01:49:12 +0900

    새로운 변수형 object
    object는 마법의 변수형이었다!
    어떤 변수를 다 집어넣을 수 있었다!

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace Use_data
 	{
 		public static void Main (string[] args)
 		{
-			bool a = true;
-			bool b = false;
+			object a = 123;
+			object b = 3.141592;
+			object c = true;
+			object d = "안녕하세요.";
 
 			Console.WriteLine (a);
 			Console.WriteLine (b);
+			Console.WriteLine (c);
+			Console.WriteLine (d);
 		}
 	}
 }
