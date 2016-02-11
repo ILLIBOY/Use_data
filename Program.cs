92f64d197edc993457a3f307c9e1817a2afbe5af
ILLIBOY <illionairedan@ssem.re.kr>
2016-02-11 16:38:44 +0900

    있음 -> 없음하면 깨진다.

--- Program.cs
+++ Program.cs
@@ -6,21 +6,17 @@ namespace Use_data
 	{
 		public static void Main (string[] args)
 		{
-			float a = 69.6875f;
-			Console.WriteLine ("a : {0}", a);
+			int a = 500;
+			Console.WriteLine (a);
 
-			double b = (double)a;
-			Console.WriteLine ("b : {0}", b);
-		
-			Console.WriteLine ("69.6875 == b : {0}", 69.6875 == b);
+			uint b = (uint)a;
+			Console.WriteLine (b);
 
-			float x = 0.1f;
-			Console.WriteLine ("x : {0}", x);
+			int x = -30;
+			Console.WriteLine (x);
 
-			double y = (double)x;
-			Console.WriteLine ("y : {0}", y);
-
-			Console.WriteLine ("0.1 == y : {0}", 0.1 == y);
+			uint y = (uint)x;
+			Console.WriteLine (y);
 		}
 	}
 }
